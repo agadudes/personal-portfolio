b10d481969e64f25406ae4f7eaeef6139cd4e295
agadudes <96642709+agadudes@users.noreply.github.com>
2024-08-05 23:48:02 +0300

    Recoded navigation bar
    Improved navigation bar responsive behavior with new CSS, media queries and Javascript.
    Added new elements to HTML.
    Selected new fonts.
    Created variables for color palatte

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+var mobileMenu = document.getElementById("mobile-nav");
+function toggleOpen(){
+  mobileMenu.style.display = "flex";
 }
+
+function toggleClose() {
+  mobileMenu.style.display = "none";
+}
+
+function resetMobile() {
+  if (window.innerWidth >= 1000) {
+    mobileMenu.style.display = "none";
+  }
+}
+
+window.onresize = resetMobile;
+resetMobile();
